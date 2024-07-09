b751f0a4fc6e6c29fd2ee2d4aac239d95f4cba81
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-09 15:45:36 +0300

    chore: add simple example

---
+++ packages/t-kassa-api/examples/simple.ts
@@ -0,0 +1,5 @@
+import { TKassa } from "../src";
+
+const tkassa = new TKassa("", "", {
+	server: "https://rest-api-test.tinkoff.ru",
+});
---
+++ packages/t-kassa-api/tests/index.test.ts
@@ -0,0 +1,8 @@
+// import { describe, expect, test } from "bun:test";
+
+// describe("TKassa()", () => {
+// 	test("TKassa", () => {
+//         const TKassa
+// 		expect(sum(1, 1)).toBe(2);
+// 	});
+// });
