6419f92ede92c471e86bda34bde0a0177446661f
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-10 15:01:41 +0300

    fix: typings and refactor

--- src/utils.ts
+++ src/utils.ts
@@ -29,66 +29,33 @@ export type Require<O extends Record<any, any>, K extends keyof O> = {
 	[P in keyof O]-?: P extends K ? NonNullable<O[P]> : O[P];
 };
 
-export type Expand<T> = T extends (...args: infer A) => infer R
-	? (...args: A) => R
-	: T extends object
-		? T extends infer O
-			? { [K in keyof O]: O[K] }
-			: never
-		: T;
-
-export type ExpandRecursively<T> = T extends (...args: infer A) => infer R
-	? (...args: ExpandRecursively<A>) => ExpandRecursively<R>
-	: T extends object
-		? T extends infer O
-			? { [K in keyof O]: ExpandRecursively<O[K]> }
-			: never
-		: T;
-
-function a(
-	b: Expand<
-		paths["/v2/TinkoffPay/terminals/{TerminalKey}/status"]["get"]["parameters"]["path"]["TerminalKey"]
-	>,
-) {}
-
-type A = Expand<
-	paths["/v2/TinkoffPay/terminals/{TerminalKey}/status"]["get"]["parameters"]["path"]["TerminalKey"]
->;
-
-type Prettify<T> = T extends object ? {} & { [P in keyof T]: T[P] } : {} & T;
-
-type B =
-	paths["/v2/TinkoffPay/terminals/{TerminalKey}/status"]["get"]["parameters"]["path"]["TerminalKey"];
-
-type AA = NonNullable<
-	paths["/v2/getConfirmOperation"]["post"]["requestBody"]
->["content"]["application/json"];
-
-type BB = paths["/v2/getConfirmOperation"]["post"]["responses"];
-
 export type GetRequestBody<
 	Path extends keyof paths,
 	Method extends "get" | "post",
-> = paths[Path][Method] extends { requestBody?: { content: any } }
-	? Omit<
-			NonNullable<
-				paths[Path][Method]["requestBody"]
-			>["content"]["application/json"],
-			"Token" | "TerminalKey"
-		>
+> = paths[Path] extends { [K in Method]: any }
+	? paths[Path][Method] extends { requestBody?: { content: any } }
+		? Omit<
+				NonNullable<
+					paths[Path][Method]["requestBody"]
+				>["content"]["application/json"],
+				"Token" | "TerminalKey"
+			>
+		: never
 	: never;
 
 export type GetResponse<
 	Path extends keyof paths,
 	Method extends "get" | "post",
-> = paths[Path][Method] extends {
-	responses: {
-		200: {
-			content: {
-				"application/json": any;
+> = paths[Path] extends { [K in Method]: any }
+	? paths[Path][Method] extends {
+			responses: {
+				200: {
+					content: {
+						"application/json": any;
+					};
+				};
 			};
-		};
-	};
-}
-	? paths[Path][Method]["responses"][200]["content"]["application/json"]
+		}
+		? paths[Path][Method]["responses"][200]["content"]["application/json"]
+		: never
 	: never;
